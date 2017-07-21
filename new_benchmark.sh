05f76e4122fd73c54415ae247d38278d123d5016
Christoph <matheja@cs.rwth-aachen.de>
2017-07-21 09:38:11 +0200

    script to generate a new benchmark project

---
+++ new_benchmark.sh
@@ -0,0 +1,6 @@
+mvn archetype:generate \
+  -DarchetypeGroupId=de.rwth.i2.attestor.examples \
+  -DarchetypeVersion=0.1 \
+  -DarchetypeArtifactId=benchmark \
+  -DarchetypeRepository=https://raw.githubusercontent.com/cmatheja/mvn-repo/master
+
