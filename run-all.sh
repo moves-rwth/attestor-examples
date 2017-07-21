6e0989eb212326c1bcae7e3a47e11ff2dacf2a8c
Christoph <matheja@cs.rwth-aachen.de>
2017-07-21 14:27:07 +0200

    updated script to run all benchmarks

--- run-all.sh
+++ run-all.sh
@@ -2,11 +2,12 @@ RESULTS=results.text
 FILE=jmh-result.text
 
 rm -f $RESULTS
-
 for D in `find . -maxdepth 1 -mindepth 1 -not -path '*/\.*' -type d`
 do
     cd $D
-        mvn clean install exec:exec@run
+    mvn clean install exec:exec@run
+    cat $FILE >> ../$RESULTS
+    rm -f $FILE
     cd ..
 done
-
+echo "All results have been written to $RESULTS."
