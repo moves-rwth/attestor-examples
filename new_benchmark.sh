a46d6fefb146b399dcf19776810cf843b64c9b76
Christoph <matheja@cs.rwth-aachen.de>
2017-07-21 13:50:18 +0200

    updated benchmark script to new archetype version

--- new_benchmark.sh
+++ new_benchmark.sh
@@ -1,6 +1,6 @@
 mvn archetype:generate \
   -DarchetypeGroupId=de.rwth.i2.attestor.examples \
-  -DarchetypeVersion=0.1 \
+  -DarchetypeVersion=0.2 \
   -DarchetypeArtifactId=benchmark \
   -DarchetypeRepository=https://raw.githubusercontent.com/cmatheja/mvn-repo/master
 
