733a9fbf1a00097cc9671fe74e2442e4dc03d717
Christoph <matheja@cs.rwth-aachen.de>
2017-08-28 08:13:34 +0200

    daily build script now reports runtimes

--- .daily_build.sh
+++ .daily_build.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+# temporary file to collect all results
+RESULTS=results.text
+
+# stop execution on first fail
 set -e
 
 runTest() {
     cd $1
     mvn clean install exec:exec@run
+    cat jmh-result.text >> ../$RESULTS
     cd ..
 }  
+
+rm $RESULTS
 
 runTest sllist
 runTest RefSLL
@@ -15,3 +22,5 @@ runTest RefBTDLL
 runTest RefBT
 runTest bimap
 runTest avltree
+
+cat $RESULTS
