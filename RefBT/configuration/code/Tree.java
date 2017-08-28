81f38ded9d614b67acb79b285a37babd5bcd99f7
Christina Jansen <28920918+ChrJansen@users.noreply.github.com>
2017-08-28 12:22:31 +0200

    Restructured class layout (now possible because of fixed duplicate type soot bug).

--- RefBT/configuration/code/Tree.java
+++ RefBT/configuration/code/Tree.java
@@ -1,41 +1,5 @@
-public class Tree{
-
+class Tree{
+	
 	Tree right;
 	Tree left;
-		
-	static void traverseLindstrom(Tree root){
-		
-		if(root == null) return;
-		Tree sen = new Tree();		
-		
-		Tree prev = sen;
-		Tree cur  = root;
-		
-		while(cur != sen){
-			Tree next = cur.left;
-					
-			cur.left = cur.right;
-			cur.right = prev;
-			
-			prev = cur;
-			cur  = next;
-		
-			if (cur == null){
-				cur  = prev;
-				prev = null;
-			}
-		}	
-	}
-	
-	static public void traverseRecursive(Tree tree){
-		if(tree != null){
-			traverseRecursive(tree.left);
-			traverseRecursive(tree.right);
-		}
-	}
-	
-	public static void main(String[] args) {
-   
-    }
-	
 }
