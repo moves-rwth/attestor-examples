e48772596a4b64a983b44266551878d54f111da0
Christoph <matheja@cs.rwth-aachen.de>
2017-10-12 20:38:45 +0200

    added experimental benchmarks

--- RefBT/configuration/code/Tree.java
+++
@@ -1,5 +0,0 @@
-class Tree{
-	
-	Tree right;
-	Tree left;
-}
--- RefBTDLL/benchmarks.sh
+++
@@ -1,6 +0,0 @@
-RESULTS=jmh-result.text
-
-mvn clean install exec:exec@run > tmp
-cat tmp | grep "including procedure calls" >> $RESULTS
-rm tmp
-cat $RESULTS
