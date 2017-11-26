cf4914c66fdb0ef18c8b73b5a93883d93af900a2
Christoph <matheja@cs.rwth-aachen.de>
2017-11-26 11:50:26 +0100

    updated RefDLL examples.

--- RefDLL/src/main/java/refdll/RefDLL.java
+++ RefDLL/src/main/java/refdll/RefDLL.java
@@ -13,27 +13,27 @@ public class RefDLL {
     
       @Benchmark
     public void buildList() {
-        BenchmarkHelper.run( 94, 48, 4 );
+        BenchmarkHelper.run( 66, 34, 3 );
     }
     
     @Benchmark
     public void buildAndTraverse(){
-        BenchmarkHelper.run( 206, 12, 3  );
+        BenchmarkHelper.run( 127, 7, 1  );
     }
 	
 	@Benchmark
 	public void MC_Completeness_traverseWithInitialList(){
-		BenchmarkHelper.run();
+		BenchmarkHelper.run(430,430,1);
 	}
 	
 	@Benchmark
 	public void MC_NeighbourhoodPreservance_traverseWithInitialList(){
-		BenchmarkHelper.run();
+		BenchmarkHelper.run(834,834,1);
 	}
 	
 	@Benchmark
 	public void MC_Reach_traverseWithInitialList(){
-		BenchmarkHelper.run();
+		BenchmarkHelper.run(53,53,1);
 	}
 	
 	@Benchmark
@@ -43,22 +43,17 @@ public class RefDLL {
 	
 	@Benchmark
 	public void MC_Completeness_reverseWithInitialList(){
-		BenchmarkHelper.run();
+		BenchmarkHelper.run(816,816,1);
 	}
 	
 	@Benchmark
 	public void MC_NeighbourhoodPreservance_reverseWithInitialList(){
-		BenchmarkHelper.run();
-	}
-	
-    @Benchmark
-	public void MC_NeighbourhoodPreservance_reverseWithInitialList_GC(){
-		BenchmarkHelper.run();
+		BenchmarkHelper.run(1581,1581,1);
 	}
 	
 	@Benchmark
 	public void MC_Reach_reverseWithInitialList(){
-		BenchmarkHelper.run();
+		BenchmarkHelper.run(114,114,1);
 	}
 	
 	@Benchmark
