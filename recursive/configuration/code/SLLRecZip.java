4b444d27be2100b2e135206a816f13da130890c8
Christoph <matheja@cs.rwth-aachen.de>
2018-01-27 08:35:21 +0100

    Updated zip method.

--- recursive/configuration/code/SLLRecZip.java
+++ recursive/configuration/code/SLLRecZip.java
@@ -1,18 +1,58 @@
-public class SLLRecBuild {
+public class SLLRecZip {
 
     public static void main(String[] args) {
 
-        SLL list = buildList(args.length, null);
+        SLL left = buildList(args.length);
+        SLL right = buildList(args.length);
+        SLL zipped = zip(left, right);
     }
 
-    private static SLL buildList(int argLength, SLL list) {
+    private static SLL buildList(int argsLength) {
 
-            if(argLength < 42) { // nondeterminism
-                SLL nextElement = new SLL();
-                nextElement.next = list;
-                return buildList(argLength, nextElement);
-            }
-            return list;
+        SLL x = null;
+        SLL y = null;
+
+        for (int i = 0; i < argsLength; i++) { // resolved by nondeterminism
+            y = new SLL();
+            y.next = x;
+            x = y;
+        }
+
+        return x;
     }
+
+    private static SLL zip(SLL left, SLL right) {
+
+        if(left == null)  {
+            return right;
+        }
+
+        SLL first = left;
+        return zipRight(first, first.next, right);
+    }
+
+    private static SLL zipLeft(SLL current, SLL left, SLL right) {
+
+        if(left == null) {
+            current.next = right;
+            return current;
+        } else {
+            current.next = left;
+            return zipRight(left, left.next, right);
+        }
+    }
+
+    private static SLL zipRight(SLL current, SLL left, SLL right) {
+
+        if(right == null) {
+            current.next = left;
+            return current;
+        } else {
+            current.next = right;
+            return zipLeft(right, left, right.next);
+        }
+    }
+
+
 
 }
