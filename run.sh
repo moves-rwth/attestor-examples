33031bac7a67a8927af3efe12b20d37f85e7a266
Christoph <matheja@cs.rwth-aachen.de>
2018-03-20 16:44:13 +0100

    adapted benchmark script

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 CSVFILE=benchmark-results.csv
 TEXFILE=benchmark-results.tex
 rm $CSVFILE
-echo "name,property,states,generation,mc,verification,total" > $CSVFILE
+echo "name,states,generation,mc,verification,total" > $CSVFILE
 mvn clean install exec:exec@run
 
 
@@ -23,7 +23,7 @@ cat <<"TAGTEXTFILE" > $TEXFILE
         \bfseries Benchmark & \bfseries No. States & \bfseries Statespace Gen. (s) & \bfseries Model checking (s) & \bfseries Total Verification (s) & \bfseries Total Runtime (s)
         \\ \hline \hline
         \csvreader[head to column names]{benchmark-results.csv}{}
-        {\name~(\property) & \states & \generation & \mc & \verification & \total \\}
+        {\name & \states & \generation & \mc & \verification & \total \\}
                & & & & & \\ \hline
     \end{longtable}
 \end{document}
